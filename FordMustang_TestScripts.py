e6e33169de2a72f43a80bd6fd40063b9fce2b665
garytsokatos <54143076+garytsokatos@users.noreply.github.com>
2019-09-25 11:53:52 -0500

    completed test cases, currently commenting my code, still need to fix xpaths and fix certain buttons that I had skipped over, includes all test scripts

--- FordMustang_TestScripts.py
+++ FordMustang_TestScripts.py
@@ -31,25 +31,113 @@ def Test_C01():
 def Test_C02():
 
         TestModule.powertrain()
+        TestModule.engine()
+        TestModule.transmissions()
 
 def Test_C03():
 
         TestModule.packages()
+        TestModule.equipment_group()
+        TestModule.exterior_package()
+        TestModule.interior_package()
 
 def Test_C04():
 
         TestModule.exterior()
+        TestModule.wheel_type()
+        TestModule.exterior_options()
+        TestModule.tire_type()
+        TestModule.rear_axle_ratios()
 
 def Test_C05():
 
         TestModule.interior()
+        TestModule.cloth()
+        TestModule.interior_options()
+        TestModule.radio_type()
+        TestModule.audio_upgrade()
+
+def Test_D01():
+
+        TestModule.summary()
+        #TestModule.special_offers()
+        #TestModule.trade_in_value()
+        TestModule.search_inventory()
+
+def Test_E01():
+
+        TestModule.let_us_find_it_for_you_select()
+        TestModule.let_us_find_it_for_you_your_model()
+        TestModule.let_us_find_it_for_you_exterior_colors()
+        TestModule.let_us_find_it_for_you_interior_colors()
+        TestModule.let_us_find_it_for_you_your_preferences()
+        TestModule.let_us_find_it_for_you_optional_upgrades()
+
+def Test_F01():
+
+        TestModule._continue_()
+
+def Test_F02():
+
+        TestModule.change_dealer_mileage()
+        TestModule.change_dealer_zipcode()
+        TestModule.select_from_list_of_dealers()
+        TestModule.body_style()
+        TestModule.select_model()
+        TestModule.year()
+
+def Test_F03():
+
+        TestModule.power_and_handling()
+
+def Test_F04():
+
+        TestModule.exterior_color()
+
+def Test_F05():
+
+        TestModule.interior_color()
+
+def Test_F06():
+
+        TestModule.exterior_features()
+
+def Test_F07():
+
+        TestModule.let_us_find_your_vehicle()
+
+def Test_G01():
+
+        TestModule.view_details()
+
+def Test_G02():
+
+        TestModule.get_internet_price()
+
+def Test_G03():
+
+        TestModule.schedule_test_drive()
+
 
 Test_A01_A06()
 Test_B01()
-Test_C01()
-Test_C02()
-Test_C03()
-Test_C04()
-Test_C05()
+#Test_C01()
+#Test_C02()
+#Test_C03()
+#Test_C04()
+#Test_C05()
+Test_D01()
+#Test_E01()
+Test_F01()
+Test_F02()
+Test_F03()
+Test_F04()
+Test_F05()
+Test_F06()
+Test_F07()
+Test_G01()
+Test_G02()
+Test_G03()
+print("closing browser")
 time.sleep(5)
 TestModule.close_browser()
